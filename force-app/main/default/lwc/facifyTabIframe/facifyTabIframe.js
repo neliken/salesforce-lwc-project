a0ce4861f4bc271029497b7df71a5a9327aa4e7a
Damian Dan <dandamian646@gmail.com>
2025-07-01 11:19:46 +0300

    iFrame fix

--- force-app/main/default/lwc/facifyTabIframe/facifyTabIframe.js
+++ force-app/main/default/lwc/facifyTabIframe/facifyTabIframe.js
@@ -1,33 +1,44 @@
-import { LightningElement, wire } from 'lwc';
-import getAnyContactId from '@salesforce/apex/FacifySalesforceHelper.getAnyContactId';
-import getOrgId from '@salesforce/apex/FacifySalesforceHelper.getOrgId';
-import USER_ID from '@salesforce/user/Id';
+import { LightningElement, wire } from "lwc";
+import getAnyContactId from "@salesforce/apex/FacifySalesforceHelper.getAnyContactId";
+import getOrgId from "@salesforce/apex/FacifySalesforceHelper.getOrgId";
+import getOrgName from "@salesforce/apex/FacifySalesforceHelper.getOrgName";
+import USER_ID from "@salesforce/user/Id";
 
 export default class FacifyTabIframe extends LightningElement {
-    contactId;
-    orgId;
+  contactId;
+  orgName;
+  orgId;
 
-    @wire(getAnyContactId)
-    wiredContact({ data, error }) {
-        if (data) {
-            this.contactId = data;
-        } else {
-            console.error('Error fetching contact:', error);
-        }
+  @wire(getAnyContactId)
+  wiredContact({ data, error }) {
+    if (data) {
+      this.contactId = data;
+    } else {
+      console.error("Error fetching contact:", error);
     }
+  }
 
-    @wire(getOrgId)
-    wiredOrgId({ data, error }) {
-        if (data) {
-            this.orgId = data;
-        } else {
-            console.error('Error fetching org ID:', error);
-        }
+  @wire(getOrgId)
+  wiredOrgId({ data, error }) {
+    if (data) {
+      this.orgId = data;
+    } else {
+      console.error("Error fetching org ID:", error);
     }
+  }
 
-    get computedUrl() {
-        if (!this.contactId || !this.orgId) return '';
-
-        return `https://api.thefacesgroup.org/api/v1/salesforce/oauth/start?contactId=${this.contactId}&userId=${USER_ID}&orgId=${this.orgId}`;
+  @wire(getOrgName)
+  wiredOrgName({ data, error }) {
+    if (data) {
+      this.orgName = data;
+    } else {
+      console.error("Error fetching org name:", error);
     }
+  }
+
+  get computedUrl() {
+    if (!this.contactId || !this.orgId || !this.orgName) return "";
+
+    return `https://nelly-dev-ed.thefacesgroup.org/login/auth?contactId=${this.contactId}&userId=${USER_ID}&orgId=${this.orgId}`;
+  }
 }
