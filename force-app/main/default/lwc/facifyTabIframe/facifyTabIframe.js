91770fde8d3ab10583946c80355f915ac06e1d74
Damian Dan <dandamian646@gmail.com>
2025-07-02 12:33:36 +0300

    feature/197-remove-apex-class

--- force-app/main/default/lwc/facifyTabIframe/facifyTabIframe.js
+++ force-app/main/default/lwc/facifyTabIframe/facifyTabIframe.js
@@ -1,44 +1,27 @@
 import { LightningElement, wire } from "lwc";
-import getAnyContactId from "@salesforce/apex/FacifySalesforceHelper.getAnyContactId";
-import getOrgId from "@salesforce/apex/FacifySalesforceHelper.getOrgId";
-import getOrgName from "@salesforce/apex/FacifySalesforceHelper.getOrgName";
 import USER_ID from "@salesforce/user/Id";
+import { CurrentPageReference } from "lightning/navigation";
 
 export default class FacifyTabIframe extends LightningElement {
   contactId;
-  orgName;
-  orgId;
 
-  @wire(getAnyContactId)
-  wiredContact({ data, error }) {
-    if (data) {
-      this.contactId = data;
-    } else {
-      console.error("Error fetching contact:", error);
+  @wire(CurrentPageReference)
+  getStateParameters(currentPageReference) {
+    if (currentPageReference) {
+      this.contactId = currentPageReference.attributes.recordId;
     }
   }
 
-  @wire(getOrgId)
-  wiredOrgId({ data, error }) {
-    if (data) {
-      this.orgId = data;
-    } else {
-      console.error("Error fetching org ID:", error);
-    }
-  }
-
-  @wire(getOrgName)
-  wiredOrgName({ data, error }) {
-    if (data) {
-      this.orgName = data;
-    } else {
-      console.error("Error fetching org name:", error);
-    }
+  get orgName() {
+    const host = window.location.hostname;
+    return host.split(".")[0];
   }
 
   get computedUrl() {
-    if (!this.contactId || !this.orgId || !this.orgName) return "";
+    const orgName = this.orgName;
 
-    return `https://nelly-dev-ed.thefacesgroup.org/auth/login?contactId=${this.contactId}&userId=${USER_ID}&orgId=${this.orgId}`;
+    if (!this.contactId || !USER_ID) return "";
+
+    return `https://${orgName}.thefacesgroup.org/auth/login?contactId=${this.contactId}&userId=${USER_ID}&orgId=${this.orgId}`;
   }
 }
